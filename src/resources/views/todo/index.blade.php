a58e8bea9ea814afad72ff8de48dfbb45671bfdd
shota suzuki <suzusho956@yahoo.co.jp>
2025-05-07 12:02:13 +0900

    add: 新規作成画面追加(create)

--- src/resources/views/todo/index.blade.php
+++ src/resources/views/todo/index.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -10,8 +11,9 @@
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
 </head>
+
 <body>
-<div id="app">
+  <div id="app">
     <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
       <div class="container">
         <a class="navbar-brand" href="/todo">ToDo</a>
@@ -28,22 +30,26 @@
       <div class="container">
         <div class="row justify-content-center">
           <div class="col-md-8">
+            <p class="text-left">
+              <a class="btn btn-success" href="http://localhost:8080/todo/create">ToDoを追加</a>
+            </p>
             <div class="card">
               <div class="card-header">
                 ToDo一覧
               </div>
               <div class="list-group list-group-flush">
                 @foreach ($todos as $todo)
-                  <div class="d-flex align-items-center p-2">
-                    <span class="col-9">{{ $todo->content }}</span>
-                  </div>
+                <div class="d-flex align-items-center p-2">
+                  <span class="col-9">{{ $todo->content }}</span>
+                </div>
                 @endforeach
               </div>
             </div>
           </div>
         </div>
-     </div>
+      </div>
     </main>
   </div>
 </body>
+
 </html>
